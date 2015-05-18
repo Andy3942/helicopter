084118350be06538d0a07a88571046e850f78f2a
Andy3942 <aa394215561@gmail.com>
2015-05-18 18:55:15 +0800

    “”

---
+++ Assets/Codes/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Application;
+
+public class GameManager : MonoBehaviour {
+
+	public IGameState _CurrentState;
+
+	// Use this for initialization
+	void Start () {
+		_CurrentState = new GameStartState (this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_CurrentState.Update ();
+	}
+}
